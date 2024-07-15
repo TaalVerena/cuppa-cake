8053e4c099e0d3c0e89a6de0e61698f209036669
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-07-15 20:21:46 +0000

    feat(checkout): Add basic checkout functionality

--- cuppa_cake/settings.py
+++ cuppa_cake/settings.py
@@ -187,5 +187,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
