62baa915e59b681ec9376e89fb06763b42e38156
S Bester <sarahleebester@gmail.com>
2024-07-21 23:05:47 +0100

    maint(bag): Refactor

--- bag/views.py
+++ bag/views.py
@@ -33,7 +33,7 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount """
+    """ Adjust the quantity of the product to the specified amount """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
