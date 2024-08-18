3c6d60c1652bb6d6c14bea6b569002024d016637
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-08-18 11:45:54 +0000

    feat(newsletter): Add message & redirect

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def subscribe_view(request):
-    return render(request, 'newsletter/subscription.html')
+    subscribed = False
+
+    if request.method == "POST":
+        subscribed = True
+
+    return render(request, "newsletter/subscription.html", {"subscribed": subscribed})
