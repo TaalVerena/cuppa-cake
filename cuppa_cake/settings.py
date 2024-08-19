009065f3e0d85cc23c06a56642810a3d79f5b927
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-08-19 20:51:57 +0000

    style(home): Add colour to hero section & sections

--- cuppa_cake/settings.py
+++ cuppa_cake/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = Home
 
 ALLOWED_HOSTS = [
     "8000-taalverena-cuppacake-qe8hmj5tjug.ws-eu115.gitpod.io",
