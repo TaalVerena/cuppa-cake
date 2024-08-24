cd057057f9829337cecd516e519582edd1adafb9
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-08-24 10:46:42 +0000

    Maint(Settings): Adjust debug settings

--- cuppa_cake/settings.py
+++ cuppa_cake/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = [
     "8000-taalverena-cuppacake-qe8hmj5tjug.ws-eu115.gitpod.io",
